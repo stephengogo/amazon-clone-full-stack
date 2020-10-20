84bdae206d7d05d6561907f0ce3f45650c941ec0
stephengogo <44884901+stephengogo@users.noreply.github.com>
2020-10-20 10:33:48 -0700

    5. Adding the Basket functionality

--- amazon-clone/src/reducer.js
+++ amazon-clone/src/reducer.js
@@ -10,8 +10,13 @@ export const initialState = {
   const reducer = (state, action) => {
     console.log(action);
     switch (action.type) {
-      default:
-        return state;
+        case "ADD_TO_BASKET":
+            return {
+                ...state,
+                basket: [...state.basket, action.item],
+            };
+        default:
+            return state;
     }
   };
   
