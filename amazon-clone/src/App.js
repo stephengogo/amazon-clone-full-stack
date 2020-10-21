843ed374e2eb8d0c13d348edbc4a008f2af8e718
stephengogo <44884901+stephengogo@users.noreply.github.com>
2020-10-20 19:22:09 -0700

    7. Setting up Authentication 8. Basket

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import Header from './Header';
+import Login from './Login';
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider"
+import Checkout from './Checkout';
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+  
   return (
     <div className="App">
       <Router>
         <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/">
             <Header />
             <Home />
---
+++ amazon-clone/src/Login.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import './Login.css'
+import { Link, useHistory } from "react-router-dom";
+import { auth } from "./firebase";
+
+function Login() {
+    const history = useHistory();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signOn = e => {
+        e.preventDefault(); //to prevent the page from reloading because of the form tag.
+
+        auth
+            .signInWithEmailAndPassword(email,password)
+            .then(auth => {
+                // The useHistory() is a hook from react-router-dom which helps you to redirect user from the actual code.
+                history.push('/') 
+            })
+            .catch(error => alert(error.message))
+    }
+
+    const register = e => {
+        e.preventDefault();
+
+        auth
+            .createUserWithEmailAndPassword(email, password)
+            .then((auth) => {
+                // it successfully created a new user with email and password
+                if (auth) {
+                    history.push('/')
+                }
+            })
+            .catch(error => alert(error.message))
+    }
+
+    return (
+        <div className='login'>
+            <Link to='/'>
+                <img
+                    className="login__logo"
+                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png' 
+                />
+            </Link>
+
+            <div className='login__container'>
+                <h1>Sign-in</h1>
+
+                <form>
+                    <h5>E-mail</h5>
+                    <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
+
+                    <h5>Password</h5>
+                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
+
+                    <button type='submit' onClick={signOn} className='login__signInButton'>Sign In</button>
+                </form>
+
+                <p>
+                    By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please
+                    see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+                </p>
+
+                <button className='login__registerButton' onClick={register} >Create your Amazon Account</button>
+            </div>
+        </div>
+    )
+}
+
+export default Login
